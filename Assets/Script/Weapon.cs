3c11f7588ebb673a8bb44aca92b08a2ed60e8750
Alex <Alexander.S_97@gmx.de>
2025-02-08 18:54:51 +0100

    create bullets

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -5,9 +5,11 @@ public class Weapon : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject bullet;
     public Transform firepoint;
+    public float fireForce;
 
     public void Fire()
     {
-        Instantiate(bullet, firepoint.position, firepoint.rotation);
+        GameObject projectile = Instantiate(bullet, firepoint.position, firepoint.rotation);
+        projectile.GetComponent<Rigidbody2D>().AddForce(firepoint.up * fireForce, ForceMode2D.Impulse);
     }
 }
